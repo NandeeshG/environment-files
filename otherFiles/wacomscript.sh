b692c969a106e1f6571f66ab85dc197fc2cb98eb
NandeeshG <nandeeshgupta@gmail.com>
2021-06-01 15:11:07 +0530

    Wacom

---
+++ otherFiles/wacomscript.sh
@@ -0,0 +1,14 @@
+
+#xsetwacom --set "HID 256c:006d stylus" rotate half
+
+#change all of following to device name instead of id
+#xsetwacom set 14 button 1 key left
+#xsetwacom set 14 button 2 key up
+#xsetwacom set 14 button 3 key down
+#xsetwacom set 14 button 8 key right
+#xsetwacom set 14 button 9 key ctrl c
+#xsetwacom set 14 button 10 key ctrl z
+#xsetwacom set 14 button 11 key ctrl y
+#xsetwacom set 14 button 12 key ctrl v
+
+
